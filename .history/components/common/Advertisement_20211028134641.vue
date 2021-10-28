c7074eac71186fc1d37ab61eb48cc9db9f88c3e5
oscarhandsome <altair5555@gmail.com>
2021-10-28 18:07:59 +0300

    Remove duplicated directory

--- .history/components/common/Advertisement_20211028134641.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="container mx-auto">
-    <div class="grid grid-cols-3 gap-4">
-      <div>1</div>
-      <!-- ... -->
-      <div>9</div>
-    </div>
-  </div>
-</template>
