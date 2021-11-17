0772dde7e56cbb72295f7ebcd3a24018be02eaa0
oscarhandsome <altair5555@gmail.com>
2021-11-17 17:42:39 +0300

    fixed nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // https://nuxtjs.org/docs/get-started/commands/
-  target: 'server',
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
